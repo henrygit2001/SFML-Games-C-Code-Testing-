723d62ca92ae6ef41a856cb6fd99a347958d8e54
henrygit2001 <92187605+henrygit2001@users.noreply.github.com>
2022-05-05 01:05:55 -0700

    Create Source.cpp

---
+++ Source/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Game.h"
+using namespace sf;
+using namespace std;
+
+
+int main()
+{
+    Game game;
+    game.Circle();
+    game.isOpening();
+    return 0;
+}
